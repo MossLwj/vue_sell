2f85aeecdc84f3d9c4212fa3a740b4bb81458d15
lwj <864391385@qq.com>
2017-12-26 17:16:34 +0800

    截止慕课网高仿饿了么项目视频教程8-2部分代码，未完待续。。。

---
+++ src/components/food/food.vue
@@ -0,0 +1,43 @@
+<template>
+  <transition name="move">
+    <div v-show="showFlag" class="food" ref="food">
+
+    </div>
+  </transition>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      food: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        showFlag: false
+      };
+    },
+    methods: {
+      show() {
+        this.showFlag = true;
+      }
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .food
+    position: fixed
+    left: 0
+    top: 0
+    bottom: 48px
+    z-index: 30
+    width: 100%
+    background: #fff
+    transform: translate3d(0, 0, 0)
+    &.move-enter-active, &.move-leave-active
+      transition: all 0.2s liner
+    &.move-enter, &.move-leave-active
+      transform: translate3d(100%, 0, 0)
+</style>
